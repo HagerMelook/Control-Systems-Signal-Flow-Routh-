089a5c7a26744edc28ef08c0b0bc54b889320223
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-04-08 04:39:33 +0200

    Add files via upload

---
+++ Signal Flow Graph/Frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <Interface/>
+</template>
+
+<script>
+import Interface from './components/Interface.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Interface
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
