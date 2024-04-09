6890463bde185ee74e13867d394ddd225eda25c1
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-04-09 06:02:03 +0200

    Update Interface.vue

--- Frontend/src/components/Interface.vue
+++ Frontend/src/components/Interface.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="GUI">
-    <input type="number" min="0" max="100" ref="statesNumber">
+    <input type="number" min="2" max="100" value="2" ref="statesNumber">
     <button @click="generateStates">Generate Nodes</button>
+    <button @click="null">Set Branches</button>
+    <button @click="null">Gain Analysis</button>
   </div>
   <div ref="container">
-
   </div>
+<!-- 
+  <div id="graph" @load="init" style="height:100px; widows:100px;">
+
+  </div> -->
+
 </template>
 
 <script>
 
 import axios from 'axios'
 import Konva from 'konva'
+// import Go from 'go'
 
 export default {
   data(){
     return{
       nodes : [],
       nodeXY :[200,200],
+      go : null,
     }
   },
 
   methods:{
     
 
+    // init(){
+      
+    //   // graph = go(graph,"graph");
+    // },
 
     generateStates(){
       let numberOfNodes = this.$refs.statesNumber.value;
@@ -32,38 +44,39 @@ export default {
 
       this.initialize();
       for (let i=0;i<numberOfNodes;i++){
-        let t;
         let c= new Konva.Circle({
           radius : 15,
           id: i,
           fill:"black",
         });
-
+        let t=new Konva.Text({
+          fontSize: 15,
+          fontFamily: 'Calibri',
+          width: 25,
+          align: 'center',
+          x: c.x()-13,
+          y: this.nodeXY[1]-56,
+        });
         if (i==0||i==numberOfNodes-1){
           c.fill("white");
           c.stroke("black");
-          t=new Konva.Text({
-            fontSize: 15,
-            fontFamily: 'Calibri',
-            width: 25,
-            align: 'center',
-            x: c.x()-13,
-            y: this.nodeXY[1]-56,
-          });
           if(i==0) t.text("C(S)");
           else t.text("R(S)"); 
-          this.nodes[i]= new Konva.Group({
-          });
-          this.nodes[i].add(c);
-          this.nodes[i].add(t);
         }
         else{
-          this.nodes[i]=c;
+          t.stroke("white")
+          t.text(i);
         }
-        this.nodes[i].x(this.nodeXY[0]=this.nodeXY[0]+50),
-        this.nodes[i].y(this.nodeXY[1]),
-        this.nodes[i].draggable(true),
-        this.nodes[i].id(i),
+        let g=new Konva.Group({
+          x: this.nodeXY[0]=this.nodeXY[0]+50,
+          y: this.nodeXY[1],
+          draggable: true,
+          id: i,
+        });
+        g.add(c).add(t);
+        this.nodes[i]=g;
+        this.nodes[i].add(c);
+        this.nodes[i].add(t);
         this.layer.add(this.nodes[i]);
       }
     },
